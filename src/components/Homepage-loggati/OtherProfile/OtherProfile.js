1e74c733ff18d24d6aa8f982ddbbb8a6a5cf2e51
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2024-01-19 23:51:17 +0100

    aggiornato segui già

--- src/components/Homepage-loggati/OtherProfile/OtherProfile.js
+++ src/components/Homepage-loggati/OtherProfile/OtherProfile.js
@@ -1,12 +1,19 @@
-import { Container, Image, Spinner, Col } from "react-bootstrap";
+import { Container, Image, Spinner, Col, Button } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "./styles.css";
 
 const OtherProfile = () => {
   const storedUserId = localStorage.getItem("userId");
   const storedUserToken = localStorage.getItem("token");
   const [loading, setLoading] = useState(true);
+  const [loggedUser, setLoggedUser] = useState({});
   const [user, setUser] = useState({});
+  const [friend, setFriend] = useState("");
+  const [friendId, setFriendId] = useState("");
+  const [userFriends, setUserFriends] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   const [isMounted, setIsMounted] = useState(true);
   const { userId } = useParams();
@@ -14,7 +21,29 @@ const OtherProfile = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        // Ottenere i dati dell'utente loggato
         const userResponse = await fetch(
+          `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/${storedUserId}`,
+          {
+            headers: {
+              Authorization: "Bearer " + storedUserToken,
+            },
+          }
+        );
+
+        if (userResponse.ok) {
+          const userData = await userResponse.json();
+          console.log("userData is: ", typeof userData);
+
+          // Aggiorna lo stato friends con gli amici dell'utente loggato
+          const userFriends = userData.friendId || [];
+          setFriends(userFriends);
+        } else {
+          console.log("Error fetching user data:", userResponse.statusText);
+        }
+
+        // Ottenere i dati dell'utente visualizzato
+        const otherUserResponse = await fetch(
           `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/${userId}`,
           {
             headers: {
@@ -23,14 +52,15 @@ const OtherProfile = () => {
           }
         );
 
-        if (isMounted && userResponse.ok) {
-          const data = await userResponse.json();
+        if (isMounted && otherUserResponse.ok) {
+          const data = await otherUserResponse.json();
           console.log("data is:", data);
           setUser(data);
-
-          console.log("userId is:", userId);
         } else {
-          console.log("Error fetching user data:", userResponse.statusText);
+          console.log(
+            "Error fetching user data:",
+            otherUserResponse.statusText
+          );
         }
       } catch (error) {
         console.log("Error fetching data:", error);
@@ -47,7 +77,105 @@ const OtherProfile = () => {
     return () => {
       setIsMounted(false);
     };
-  }, [userId, storedUserToken, isMounted]);
+  }, [userId, storedUserId, storedUserToken, isMounted]);
+
+  const segui = async () => {
+    const storedUserId = localStorage.getItem("userId");
+    const storedUserToken = localStorage.getItem("token");
+    setLoading(true);
+
+    try {
+      // Ottieni gli amici dell'utente loggato dal backend
+      const userResponse = await fetch(
+        `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/${storedUserId}`,
+        {
+          headers: {
+            Authorization: "Bearer " + storedUserToken,
+          },
+        }
+      );
+
+      if (userResponse.ok) {
+        const userData = await userResponse.json();
+        console.log("userData is: ", typeof userData);
+        const userFriends = userData.friendId || []; // Array degli amici dell'utente
+
+        console.log("friends is: ", typeof friends);
+
+        // Verifica se l'amico è già presente nell'array degli amici dell'utente
+        let isFriendAlreadyAdded = false;
+
+        for (const userObj of userFriends) {
+          for (const key in userObj) {
+            if (userObj.hasOwnProperty(key) && userObj[key] === userId) {
+              isFriendAlreadyAdded = true;
+              break;
+            }
+          }
+
+          if (isFriendAlreadyAdded) {
+            break;
+          }
+        }
+        console.log(isFriendAlreadyAdded);
+        if (!isFriendAlreadyAdded) {
+          // Aggiungi l'amico all'array degli amici dell'utente
+          const newFriend = [...userFriends];
+          newFriend.push(userId);
+
+          // Invia la richiesta di aggiornamento degli amici dell'utente al backend
+          const updateResponse = await fetch(
+            `${process.env.REACT_APP_BACKEND_ENDPOINT}/api/users/${storedUserId}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + storedUserToken,
+              },
+              method: "PUT",
+              body: JSON.stringify({ friendId: newFriend }),
+            }
+          );
+
+          if (updateResponse.ok) {
+            // Aggiungi l'amico all'array degli amici utilizzando push
+            setFriend(newFriend);
+          }
+
+          setFriendId(userId);
+
+          toast("Now you're following them!", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          setTimeout(() => {
+            window.location.href = `/users/${userId}`;
+          }, 2000);
+        } else {
+          // L'amico è già presente nell'array degli amici dell'utente
+          toast.warn("You're already following them!", {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        }
+      }
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -70,9 +198,31 @@ const OtherProfile = () => {
               <h5 className="font-face-CinzelDecorative mt-3">Email: </h5>
               <p>{user?.email}</p>
             </Col>
-            <Col lg={6} className="mt-5"></Col>
+            {console.log("friends:", friends)}
+            {friends && friends.some((friend) => friend._id === userId) ? (
+              <Col lg={6} className="mt-5">
+                <Button
+                  className="font-face-CinzelDecorative align-self-center"
+                  variant="dark"
+                  disabled
+                >
+                  Segui già
+                </Button>
+              </Col>
+            ) : (
+              <Col lg={6} className="mt-5">
+                <Button
+                  className="buttonAggiungi font-face-CinzelDecorative align-self-center"
+                  onClick={() => segui(userId)}
+                >
+                  Segui
+                </Button>
+              </Col>
+            )}
           </Container>
+
           <hr></hr>
+
           <Container className="container-gdl p-0">
             <h4 className="font-face-CinzelDecorative my-3">
               GDL a cui partecipa:{" "}
@@ -113,6 +263,18 @@ const OtherProfile = () => {
           <hr></hr>
         </Container>
       )}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 };
