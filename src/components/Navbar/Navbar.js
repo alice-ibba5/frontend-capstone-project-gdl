93b7d31f69cd5018db0fdbb99de2027346c55137
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2024-01-28 21:38:10 +0100

    up

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,11 +13,20 @@ import "./styles.css";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import { startTransition } from "react";
 
 function NavBar({ searchQuery, setSearchQuery }) {
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [logoutCompleted, setLogoutCompleted] = useState(false);
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    setUser(null);
+    setIsLoggedIn(false);
+    setLogoutCompleted(true);
+  };
 
   const isLogged = async () => {
     const storedUserId = localStorage.getItem("userId");
@@ -39,8 +48,7 @@ function NavBar({ searchQuery, setSearchQuery }) {
           setUser(userDetails);
           setIsLoggedIn(true);
         } else {
-          localStorage.clear();
-          window.location.href = "/";
+          handleLogout();
           toast("You need to login again!", {
             position: "bottom-right",
             autoClose: 5000,
@@ -61,6 +69,13 @@ function NavBar({ searchQuery, setSearchQuery }) {
     console.log("useEffect is triggered");
     isLogged();
   }, []);
+
+  // Effettua la navigazione solo se l'utente è disconnesso
+  useEffect(() => {
+    if (!isLoggedIn && logoutCompleted) {
+      navigate("/", { replace: true }); // Sostituisci con la tua pagina di login
+    }
+  }, [isLoggedIn, logoutCompleted, navigate]);
 
   return (
     <Navbar
@@ -117,10 +132,9 @@ function NavBar({ searchQuery, setSearchQuery }) {
                 variant="danger"
                 className="my-3 mx-3"
                 onClick={() => {
-                  navigate("/");
-                  localStorage.clear();
-                  setUser(false);
-                  setIsLoggedIn(false);
+                  startTransition(() => {
+                    handleLogout();
+                  });
                 }}
               >
                 Logout
